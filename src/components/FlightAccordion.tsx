683fbe1259ff96f1eae0a1a317ca1d644f87ce8c
Epyuel <epdemrew@gmail.com>
2025-04-19 17:24:54 +0300

    Finalised

---
+++ src/components/FlightAccordion.tsx
@@ -0,0 +1,172 @@
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  Box,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AirlineSeatLegroomReducedIcon from '@mui/icons-material/AirlineSeatLegroomReduced';
+import UsbIcon from '@mui/icons-material/Usb';
+import PublicIcon from '@mui/icons-material/Public';
+import dayjs from 'dayjs';
+import weekday from 'dayjs/plugin/weekday';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import { useState } from 'react';
+
+dayjs.extend(weekday);
+dayjs.extend(localizedFormat);
+
+export interface Flight {
+    airlineLogo: string;
+    airlineName: string;
+    departureTime: string;   
+    arrivalTime: string;       
+    durationInMinutes: number;
+    originId: string;
+    destinationId: string;
+    origin: string;
+    destination: string;
+    originCity: string;
+    destinationCity: string;
+    stops: string;        
+    price: string;            
+}
+
+const FlightAccordion = ({flight}:{flight:Flight}) => {
+  const departureTime = dayjs(flight.departureTime).format('hh:mm A');  
+  const arrivalTime = dayjs(flight.arrivalTime).format('hh:mm A');  
+  const [isOpen,setIsOpen] = useState(false);
+  return (
+    <Accordion 
+      onChange={(_,expanded)=>setIsOpen(!expanded)}
+      sx={{
+        boxShadow: 'none',
+        border: '1px solid #E2E8F0',
+        borderRadius: '12px !important',
+        '&:before': {
+          display: 'none',
+        },
+        '&.Mui-expanded': {
+          margin: 0,
+        },
+        backgroundColor: '#FFFFFF',
+      }}
+    >
+      <AccordionSummary
+        expandIcon={
+          <ExpandMoreIcon sx={{ 
+            color: '#9b87f5',
+            transform: 'scale(1.2)',
+            transition: 'transform 0.3s ease-in-out',
+            '.Mui-expanded &': {
+              transform: 'rotate(180deg) scale(1.2)',
+            }
+          }} />
+        }
+        sx={{
+          backgroundColor: '#FFFFFF',
+          borderRadius: '12px',
+          '&.Mui-expanded': {
+            borderBottomLeftRadius: 0,
+            borderBottomRightRadius: 0,
+            borderBottom: '1px solid #E2E8F0',
+          },
+          minHeight: '104px',
+          '& .MuiAccordionSummary-content': {
+            margin: '12px 0',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }
+        }}
+      >
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+          <img src={flight.airlineLogo} alt="KLM" style={{ width: '32px', height: '32px' }} />
+          <Box>
+            <Typography 
+              sx={{ 
+                fontWeight: 500,
+                fontSize: '16px',
+                color: '#1A1F2C',
+                fontFamily: '"Inter", sans-serif'
+              }}
+            >
+              { isOpen ? `${departureTime} - ${arrivalTime}`: `Return · ${dayjs(flight.arrivalTime).format('ddd, MMM D')}`}
+            </Typography>
+          </Box>
+        </Box>
+        <Typography sx={{ color: '#1A1F2C' }}>{}</Typography>
+        <Box sx={{ textAlign: 'left' }}>
+            <Typography sx={{ color: '#1A1F2C' }}>{formatDuration(flight.durationInMinutes)}</Typography>
+            <Typography sx={{ fontSize: '14px', color: '#8E9196' }}>{flight.originId} - {flight.destinationId}</Typography>
+          </Box>
+        <Typography sx={{ color: '#1A1F2C' }}>{flight.stops}</Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2,marginRight:'1rem' }}>
+          <Box sx={{ textAlign: 'right' }}>
+            <Typography sx={{ fontWeight: 500, color: '#1A1F2C' }}>{flight.price}</Typography>
+            <Typography sx={{ fontSize: '14px', color: '#8E9196' }}>round trip</Typography>
+          </Box>
+        </Box>
+      </AccordionSummary>
+      <AccordionDetails
+        sx={{
+          padding: '16px 24px',
+          backgroundColor: '#FFFFFF',
+          borderBottomLeftRadius: '12px',
+          borderBottomRightRadius: '12px',
+          color: '#8E9196',
+          fontSize: '14px',
+          lineHeight: '1.5'
+        }}
+      >
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Box sx={{display: 'flex', gap:'0.5rem'}}>
+                <div className="flex flex-col items-center space-y-2 mt-1">
+                    <div className="w-3 h-3 m-0 rounded-full border border-gray-300"></div>
+                    <div className="w-1 h-1 m-0 rounded-full bg-gray-300"></div>
+                    <div className="w-1 h-1 m-0 rounded-full bg-gray-300"></div>
+                    <div className="w-1 h-1 m-0 rounded-full bg-gray-300"></div>
+                    <div className="w-1 h-1 m-0 rounded-full bg-gray-300"></div>
+                    <div className="w-3 h-3 m-0 rounded-full border border-gray-300"></div>
+                </div>
+                <Box>
+                    <Typography sx={{ color: '#1A1F2C', mb: 1 }}>{departureTime} · {flight.origin}</Typography>
+                    <Typography sx={{ color: '#8E9196', mb: 1.5 }}>Travel time: {formatDuration(flight.durationInMinutes)}</Typography>
+                    <Typography sx={{ color: '#1A1F2C', mb: 2 }}>{arrivalTime}· {flight.destination}</Typography>
+                    <Typography sx={{ color: '#8E9196', fontSize:'13px' }}>{flight.originCity} · Economy · Embraer 195 E2 · KL 1011</Typography>
+                    <Typography sx={{ color: '#8E9196',fontSize:'13px' }}>Plane and crew by {flight.originCity}</Typography>
+                </Box>
+            </Box>
+            <Box className='!text-sm'>
+                <span className='flex gap-2 align-middle'>
+                    <AirlineSeatLegroomReducedIcon sx={{fontSize:'17px'}}/>
+                    <Typography sx={{ color: '#8E9196', mb: 1, fontSize:'14px' }}>Below average legroom (29 in)</Typography>
+                </span>
+                <span className='flex gap-2 align-middle'>
+                    <UsbIcon sx={{fontSize:'17px'}}/>
+                    <Typography sx={{ color: '#8E9196', mb: 1, fontSize:'14px' }}>In-seat USB outlet</Typography>
+                </span>
+                <span className='flex gap-2 align-middle'>
+                    <PublicIcon sx={{fontSize:'17px'}}/>
+                    <Typography sx={{ color: '#8E9196', fontSize:'14px' }}>Emissions estimate: 50 kg CO2e</Typography>
+                </span>
+            </Box>
+          </Box>
+        </Box>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default FlightAccordion;
+
+function formatDuration(minutes: number): string {
+    const hrs = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+  
+    if (hrs && mins) return `${hrs} hr ${mins} min`;
+    if (hrs) return `${hrs} hr`;
+    return `${mins} min`;
+}
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,292 @@
+import { useState, useEffect, useCallback } from "react";
+import { SelectChangeEvent } from "@mui/material/Select";
+
+import { Search } from "@mui/icons-material";
+import axios from "axios";
+import { Dayjs } from "dayjs";
+import { Snackbar, Alert, CircularProgress } from "@mui/material";
+import debounce from 'lodash.debounce';
+import TripTypeSelector from "./TripTypeSelector";
+import PassengerSelector from "./PassengerSelector";
+import TripOptions from "./TripOptions";
+import Origin from "./origin";
+import FlightsGrid from "./FlightsResults";
+import { Flight } from "./FlightAccordion";
+export interface AirportOption {
+  id: string;
+  name: string;
+  iataCode: string;
+  skyId: string;
+}
+
+export default function SearchBar() {
+  const [trip, setTrip] = useState<string>("");
+  const [passenger, setPassenger] = useState<number>(1);
+  const [type, setType] = useState<string>("economy");
+
+  const [departure, setDeparture] = useState<Dayjs | null>(null);
+  const [returnDate, setReturnDate] = useState<Dayjs | null>(null);
+
+  const [origin, setOrigin] = useState<string | null>(null);
+  const [originInputValue, setOriginInputValue] = useState<string>("");
+  const [destinationInputValue, setDestinationInputValue] = useState<string>("");
+  const [destination, setDestination] = useState<string | null>(null);
+
+  const [flights, setFlights] = useState<Flight[]>([]);
+
+  const [originId, setOriginId] = useState<string>("");
+  const [destinationId, setDestinationId] = useState<string>("");
+  const [originOptions, setOriginOptions] = useState<AirportOption[]>([]);
+  const [destinationOptions, setDestinationOptions] = useState<AirportOption[]>([]);
+
+  const [originSkyId, setOriginSkyId] = useState<string>("");
+  const [destinationSkyId, setDestinationSkyId] = useState<string>("");
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false);
+  const [snackbarMessage, setSnackbarMessage] = useState<string>("");
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
+
+  const handleFlightType = (event: SelectChangeEvent) => {
+    console.log(event.target.value)
+    setType(event.target.value);
+  };
+
+  // Shared function to fetch airports based on input value
+  // Memoize the fetchAirports function
+  const fetchAirports = useCallback(async (query: string) => {
+    try {
+      const response = await axios.get(
+        `https://sky-scrapper.p.rapidapi.com/api/v1/flights/searchAirport?query=${query}&locale=en-US`,
+        {
+          headers: {
+            "X-RapidAPI-Host": "sky-scrapper.p.rapidapi.com",
+            "X-RapidAPI-Key": "8d8040f5bfmshcd41c133fff384fp1a3687jsne076c32cdf25",
+          },
+        }
+      );
+      return response.data.data || []; // Return empty array if no data
+    } catch (error) {
+      console.error("Error fetching airport data:", error);
+      setSnackbarMessage("Error fetching airport data.");
+      setSnackbarOpen(true);
+      return [];
+    }
+  }, []);
+
+
+  const fetchOriginOptions = useCallback(
+    debounce(async (query: string) => {
+      const options = await fetchAirports(query);
+      setOriginOptions(options);
+    }, 300), // Debounce delay of 300ms
+    [fetchAirports]
+  );
+
+
+  const fetchDestinationOptions = useCallback(
+    debounce(async (query: string) => {
+      const options = await fetchAirports(query);
+      setDestinationOptions(options);
+    }, 300),
+    [fetchAirports]
+  );
+
+  useEffect(() => {
+    if (originInputValue.length > 0) {
+      fetchOriginOptions(originInputValue);
+    }
+    if (originInputValue.length === 0) {
+      setOriginOptions([])
+    }
+  }, [originInputValue, fetchOriginOptions]);
+
+  useEffect(() => {
+    if (destinationInputValue.length > 0) {
+      fetchDestinationOptions(destinationInputValue);
+    }
+  }, [destinationInputValue, fetchDestinationOptions]);
+
+  const handleSearch = async () => {
+    // const params = {
+    //   // originSkyId: originSkyId,
+    //   // originEntityId: originId,
+    //   // destinationEntityId: destinationId,
+    //   // destinationSkyId: destinationSkyId,
+    //   // date: departure ? departure.format("YYYY-MM-DD") : "",
+    //   // returnDate: returnDate ? returnDate.format("YYYY-MM-DD") : "",
+    //   // cabinClass: 'economy',
+    //   // // cabinClass: type.toLowerCase(),
+    //   // adults: passenger,
+    //   // sortBy: "best",
+    //   // currency: "USD",
+    //   // market: "en-US",
+    //   // countryCode: "US",
+    //   originSkyId: 'LOND',
+    //   destinationSkyId: 'NYCA',
+    //   originEntityId: '27544008',
+    //   destinationEntityId: '27537542',
+    //   date: '2024-07-01',
+    //   cabinClass: 'economy',
+    //   adults: '1',
+    //   sortBy: 'best',
+    //   currency: 'USD',
+    //   market: 'en-US',
+    //   countryCode: 'US'
+    // };
+    // console.log(params,"params");
+    // try {
+    //   setIsLoading(true);
+    //   const response = await axios.get(
+    //     "https://sky-scrapper.p.rapidapi.com/api/v1/flights/searchFlights",
+    //     {
+    //       params,
+    //       headers: {
+    //         "X-RapidAPI-Key": '8d8040f5bfmshcd41c133fff384fp1a3687jsne076c32cdf25',
+    //         "X-RapidAPI-Host": "sky-scrapper.p.rapidapi.com",
+    //       },
+    //     }
+    //   );
+    //   console.log(response.data,"Data");
+    //   // setFlights(response.data);
+    //   // setTimeout(() => {
+    //   //   // setFlights(data);
+    //   //   setIsLoading(false);
+    //   // }, 3000);
+    // } catch (error) {
+    //   console.error("Error fetching flights:", error);
+    //   // setTimeout(() => {
+    //   //   setFlights(data);
+    //   //   setIsLoading(false);
+    //   // }, 3000);
+    // } finally{
+    //   setIsLoading(false);
+    // }
+    const options = {
+      method: 'GET',
+      url: 'https://sky-scrapper.p.rapidapi.com/api/v2/flights/searchFlights',
+      params: {
+        originSkyId: originSkyId,
+        destinationSkyId: destinationSkyId,
+        originEntityId: originId,
+        destinationEntityId: destinationId,
+        date: departure ? departure.format("YYYY-MM-DD") : "",
+        returnDate: returnDate ? returnDate.format("YYYY-MM-DD"):"",
+        cabinClass: type.toLowerCase(),
+        adults: `${passenger}`,
+        sortBy: 'best',
+        currency: 'USD',
+        market: 'en-US',
+        countryCode: 'US'
+      },
+      headers: {
+        'x-rapidapi-key': '8d8040f5bfmshcd41c133fff384fp1a3687jsne076c32cdf25',
+        'x-rapidapi-host': 'sky-scrapper.p.rapidapi.com'
+      }
+    };
+    
+    setIsLoading(true)
+    try {
+      const response = await axios.request(options);
+      setFlights((response?.data?.data?.itineraries?.slice(0,10)??[]).map(transformFlightData))
+    } catch (error) {
+      console.error(error);
+    }finally{
+      setIsLoading(false)
+    }
+  };
+
+  return (
+    <>
+      <div className="container mx-auto flex justify-center items-center p-2 md:p-0">
+        <div className="border border-gray-300 p-6 grid grid-cols-1 gap-6 bg-white shadow-lg rounded-lg">
+          <div className="flex flex-row md:flex-row">
+            <TripTypeSelector value={trip} onChange={setTrip} />
+            <PassengerSelector passenger={passenger} setPassenger={setPassenger} />
+            <TripOptions type={type} handleFlightType={handleFlightType} />
+          </div>
+          <Origin
+            origin={origin}
+            originInputValue={originInputValue}
+            destination={destination}
+            destinationInputValue={destinationInputValue}
+            departure={departure}
+            returnDate={returnDate}
+            originOptions={originOptions}
+            destinationOptions={destinationOptions}
+            setOrigin={setOrigin}
+            setOriginInputValue={setOriginInputValue}
+            setDestination={setDestination}
+            setDestinationInputValue={setDestinationInputValue}
+            setDeparture={setDeparture}
+            setReturnDate={setReturnDate}
+            setOriginId={setOriginId}
+            setDestinationId={setDestinationId}
+            setOriginSkyId={setOriginSkyId}
+            setDestinationSkyId={setDestinationSkyId}
+          />
+        </div>
+      </div>
+      <div className="flex justify-center mt-[-20px]">
+        <button
+          onClick={handleSearch}
+          className="flex justify-items-center px-3 gap-2 p-2 border w-28 rounded-full bg-blue-500 text-white"
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <>
+              <CircularProgress size={24} color="inherit" />
+              <p className="my-auto">{"Explore"}</p>
+            </>
+          ) : (
+            <>
+              <Search />
+              <p className="my-auto">{"Explore"}</p>
+            </>
+          )}
+        </button>
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={6000}
+          onClose={handleSnackbarClose}
+        >
+          <Alert
+            onClose={handleSnackbarClose}
+            severity="error"
+            sx={{ width: "100%" }}
+          >
+            {snackbarMessage}
+          </Alert>
+        </Snackbar>
+      </div>
+      <FlightsGrid flightsData={flights} />
+    </>
+  );
+}
+
+function transformFlightData(raw: any): any {
+  const leg = raw.legs?.at(0) || {};
+  return {
+    airlineLogo: leg?.carriers?.marketing?.at(0)?.logoUrl || "",
+    airlineName: leg?.carriers?.marketing?.at(0)?.name || "",
+    originId: leg?.origin?.id || "",
+    destinationId: leg?.destination?.id || "",
+
+    departureTime: leg?.departure || "",
+    arrivalTime: leg?.arrival || "",
+    durationInMinutes: leg?.durationInMinutes || "",
+    origin: leg?.origin?.name || "",
+    destination: leg?.destination?.name || "",
+    originCity: leg?.origin?.city??"",
+    destinationCity: leg?.destination?.city??"",
+    stops: leg?.stopCount
+      ? `${leg?.stopCount} stop${leg?.stopCount > 1 ? "s" : ""}`
+      : "Nonstop",
+    
+    price: raw?.price?.formatted || "$0",
+  };
+}
